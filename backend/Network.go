6746bdc824244cac1061714e1eea211a781d9491
yesoer <luisenglaender@web.de>
2023-10-03 16:11:14 +0200

    feat(backend): init network/nodes

---
+++ backend/Network.go
@@ -0,0 +1,100 @@
+package backend
+
+import (
+	"log"
+	"os"
+)
+
+type Signal int
+
+type Code string
+
+const (
+	START Signal = 1 // start code execution from the beginning
+	STOP  Signal = 2 // stop code execution
+)
+
+type Network interface {
+	Init(cnt int)
+	Emit(s Signal)
+	GetConnections() [][]*Connection
+	GetNodeCnt() int
+	ConnectNodes(fromId, toId int)
+	DisconnectNodes(fromId, toId int)
+	SetCode(code Code)
+	GetCode() Code
+	SetData(json interface{}, toId int)
+}
+
+func (n *network) GetCode() Code {
+	return n.Code
+}
+
+type network struct {
+	Nodes   []Node
+	Signals chan Signal
+	Code    Code
+}
+
+func NewNetwork() Network {
+	var nodes []Node
+	var code Code
+	signals := make(chan Signal, 10)
+	return &network{nodes, signals, code}
+}
+
+func (n *network) SetData(json interface{}, toId int) {
+	n.Nodes[toId].SetData(json)
+}
+
+func (n *network) SetCode(code Code) {
+	n.Code = code
+}
+
+func (n *network) ConnectNodes(fromId, toId int) {
+	n.Nodes[fromId].ConnectTo(toId)
+}
+
+func (n *network) DisconnectNodes(fromId, toId int) {
+	n.Nodes[fromId].DisconnectFrom(toId)
+}
+
+func (n *network) Init(cnt int) {
+	for i := 0; i < cnt; i++ {
+		newNode := NewNode(i)
+
+		// TODO : redundant with editor
+		// and also needs changing on code editing
+		// I think just use network.SetCode in editor
+		b, err := os.ReadFile("./code.go")
+		if err == nil {
+			n.Code = Code(b)
+		}
+
+		newNode.Run(n.Signals, n.Code)
+		n.Nodes = append(n.Nodes, newNode)
+	}
+}
+
+func (n *network) Emit(s Signal) {
+	log.Printf("Emit signal %d to nodes", s)
+	for range n.Nodes {
+		n.Signals <- s
+	}
+}
+
+// returns exactly one connections slice for each node
+func (n *network) GetConnections() [][]*Connection {
+	res := make([][]*Connection, len(n.Nodes))
+
+	for nodei, node := range n.Nodes {
+		connections := node.GetConnections()
+		res[nodei] = connections
+	}
+
+	return res
+}
+
+func (n *network) GetNodeCnt() int {
+	return len(n.Nodes)
+}
--- main.go
+++ main.go
@@ -7,4 +7,8 @@ import (
 var networkSizeFlag = flag.Int("network-size", 2, "define how many nodes you want in your network")
 
 func main() {
+	// Init backend
+	flag.Parse()
+	network := backend.NewNetwork()
+	network.Init(*networkSizeFlag)
 }
