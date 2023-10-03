fcc55bb062b77da2f74351656e187012363e0a20
yesoer <luisenglaender@web.de>
2023-10-03 17:52:50 +0200

    refactor(sample): exit gracefully

--- code.go
+++ code.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
 
-func Run(fSend func(targetId int, data any) int, fAwait func(int) int) string {
+// wait for ctx.Done to exit gracefully
+// use fSend and fAwait to communicate between nodes
+func Run(ctx context.Context, fSend func(targetId int, data any) int, fAwait func(int) int) string {
 	go func() {
 		for {
-			res := fAwait(1)
-			fmt.Println("res ", res)
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				res := fAwait(1)
+				fmt.Println("res ", res)
+			}
 		}
 	}()
 
 	for {
-
-		time.Sleep(time.Second * 5)
-		fSend(1, "data")
+		select {
+		case <-ctx.Done():
+			return "done"
+		default:
+			time.Sleep(time.Second * 5)
+			fSend(1, "data")
+		}
 	}
-	return "done"
 }
