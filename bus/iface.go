8ed2a51f1f22dce330d9f951a05846b1a0074a99
yesoer <luisenglaender@web.de>
2023-10-16 17:01:01 +0200

    feat(eventbus): define the eventbus interface
    eventtypes and data structures to be used by gui and backend/network

---
+++ bus/iface.go
@@ -0,0 +1,37 @@
+package bus
+
+// To avoid import cycles this file defines all events published aswell as their
+// data structures.
+
+//---------------------
+// PUBLISHED BY GUI
+
+// when node specific data has been changed
+var NodeDataChangeEvt EventType = "node-data-changed"
+
+type NodeDataChangeData struct {
+	TargetId int
+	Data     interface{}
+}
+
+// on (dis-)connects
+var ConnectNodesEvt EventType = "connect-nodes"
+var DisconnectNodesEvt EventType = "disconnect-nodes"
+
+type CheckboxPos struct {
+	Ccol int
+	Crow int
+}
+
+// on signals
+var StartEvt EventType = "start"
+var StopEvt EventType = "stop"
+
+// on code changes by the editor
+var CodeChangedEvt EventType = "code-changed"
+
+//---------------------
+// PUBLISHED BY BACKEND
+
+// on (dis-)connects
+var ConnectionChangeEvt EventType = "connection-changed"
