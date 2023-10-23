93735ab181843a53730fb235b34bfdf29f0503a9
yesoer <luisenglaender@web.de>
2023-10-23 20:50:32 +0200

    refactor(main): remove delay
    Not necessary due to eventbus refactoring. We now store the most recent event which can then be used by the gui components when they bind

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"distributed-sys-emulator/bus"
 	"distributed-sys-emulator/gui"
 	"flag"
-	"time"
 )
 
 func main() {
@@ -13,15 +12,8 @@ func main() {
 
 	eb := bus.NewEventbus()
 
-	// Init backend with delay
-	// The delay is because network on init publishes events
-	go func() {
-		time.Sleep(time.Millisecond * 100)
-		network := backend.NewNetwork(eb)
-		network.Init(eb)
-	}()
+	network := backend.NewNetwork(eb)
+	network.Init(eb)
 
-	// Init frontend
-	// Must run in main routine
 	gui.RunGUI(eb)
 }
