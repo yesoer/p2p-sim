7dc9de3e126d5321c55bb2d0a4a4a590485b64cf
yesoer <luisenglaender@web.de>
2023-11-08 19:55:58 +0100

    refactor(eventbus): overhaul
    - only export fields we really want to export
    - remove order, I don't think we needed due to using nested events instead, and if we do implement it differently e.g. lamport clocks ?
    - use reflect package to ensure correct callbacks and arg types, this way callbacks don't need casting
    - export an interface instead of the eventbus struct
    - implement bind and publish with await possibilities

--- bus/eventbus.go
+++ bus/eventbus.go
@@ -2,6 +2,7 @@ package bus
 
 import (
 	"log"
+	"reflect"
 	"runtime"
 	"sync"
 )
@@ -11,110 +12,165 @@ import (
 // - immediately exec the callback on bind, if an event has been published before
 // - no constantly running process
 // - callbacks may publish
-// - preserve publish order
+// - implicit eventtype/eventdata matching and checking
 
 type EventType string
+
+// callbacks are stored as any so we can store varying signatures
+// type safety is ensured through the reflect package
+type callback any
 
 type Event struct {
 	Type EventType   `json:"type"`
 	Data interface{} `json:"data"`
 }
 
-type EventBusData struct {
-	Callbacks []Callback
-	Recent    *Event
+type eventBusData struct {
+	callbacks []callback
+	recent    *Event
+	cbType    reflect.Type
 }
 
-type EventBus struct {
-	Data       map[EventType]EventBusData
-	WaitList   map[int]chan bool
-	WaitListMu sync.Mutex
-	NextId     int
-	Mu         sync.Mutex
+type EventBus interface {
+	Bind(etype EventType, cb callback)
+	AwaitBind(etype EventType, cb callback) bool
+	Publish(e Event)
+	AwaitPublish(e Event) bool
 }
 
-func NewEventbus() *EventBus {
-	data := make(map[EventType]EventBusData)
-	waitlist := make(map[int]chan bool)
-	return &EventBus{data, waitlist, sync.Mutex{}, 0, sync.Mutex{}}
+type eventBus struct {
+	data map[EventType]eventBusData
+	mu   sync.Mutex
 }
 
-type Callback func(e Event)
+func NewEventbus() EventBus {
+	data := make(map[EventType]eventBusData)
+	return &eventBus{data, sync.Mutex{}}
+}
+
+func (bus *eventBus) Bind(etype EventType, cb callback) {
+	bus.bind(etype, cb, false)
+}
+
+// returns whether the bind was successfull
+func (bus *eventBus) AwaitBind(etype EventType, cb callback) bool {
+	return bus.bind(etype, cb, true)
+}
 
 // TODO : add options to allow for only once or make that a different method ?
-// TODO : double check that this is not called in forever loops or something like that
-func (bus *EventBus) Bind(etype EventType, callback Callback) {
-	go func() {
-		bus.Mu.Lock()
-		current, ok := bus.Data[etype]
-		if !ok {
-			// first bind init EventBusData
-			callbacks := []Callback{callback}
-			bus.Data[etype] = EventBusData{callbacks, nil}
-		} else {
-			newCallbacks := append(current.Callbacks, callback)
-			bus.Data[etype] = EventBusData{newCallbacks, current.Recent}
-		}
-
-		if current.Recent != nil {
-			callback(*current.Recent)
-		}
-		log.Println("Bound func to event type : ", etype)
-		bus.Mu.Unlock()
-	}()
+// TODO : we might need to be able to unbind
+func (bus *eventBus) bind(etype EventType, cb callback, await bool) bool {
+	if await {
+		return bus.bindLogic(etype, cb)
+	} else {
+		go bus.bindLogic(etype, cb)
+	}
+	return true
 }
 
-// TODO : reuse waitlist channels
-// TODO : provide an AwaitPublish and AwaitBind which don't use go routines,
-//
-//	these should not be executed nested in a publish
-func (bus *EventBus) Publish(e Event) {
-	// wait for previous publish to finish
-	bus.WaitListMu.Lock()
-	id := bus.NextId
-	ch, ok := bus.WaitList[id]
+func (bus *eventBus) bindLogic(etype EventType, cb callback) bool {
+	// TODO : use sync map ?
+	bus.mu.Lock()
+	defer bus.mu.Unlock()
+
+	current, ok := bus.data[etype]
 	if !ok {
-		ch = make(chan bool, 10)
-		bus.WaitList[id] = ch
+		// first bind/publish to this eventtype
+		callbacks := []callback{cb}
+		cbType := reflect.TypeOf(cb)
+		bus.data[etype] = eventBusData{callbacks, nil, cbType}
+	} else {
+		// a previous bind/publish has implicitly defined which data type is
+		// expected by callbacks, check if this callbacks signature matches
+		match := current.cbType == reflect.TypeOf(cb)
+		if !match {
+			log.Println("Error : The provided callback does not match the expected arg type.")
+			return false
+		}
+		newCallbacks := append(current.callbacks, cb)
+		bus.data[etype] = eventBusData{newCallbacks, current.recent, current.cbType}
 	}
-	bus.NextId++
-	bus.WaitListMu.Unlock()
+	log.Println("Bound func to event type : ", etype)
 
-	go func() {
-		if id > 0 {
-			<-ch
+	if current.recent != nil {
+		cbv := reflect.ValueOf(cb)
+		in := []reflect.Value{}
+		if current.recent.Data != nil {
+			arg := reflect.ValueOf(current.recent.Data)
+			in = append(in, arg)
 		}
+		cbv.Call(in)
+	}
 
-		// execute all callbacks for this event before processing the next,
-		// keeping the order intact
-		bus.Mu.Lock()
-		if current, ok := bus.Data[e.Type]; !ok {
-			bus.Data[e.Type] = EventBusData{nil, &e}
-		} else {
-			current.Recent = &e
-			bus.Data[e.Type] = current
-		}
+	return true
+}
 
-		prevFile, line := getPreviousCallerFile()
-		log.Println("Publish event ", e, " from ", prevFile, ":", line)
-		if current := bus.Data[e.Type]; current.Callbacks != nil {
-			for _, cb := range current.Callbacks {
-				cb(e)
+func (bus *eventBus) Publish(e Event) {
+	bus.publish(e, false)
+}
+
+// returns whether the publish was successfull
+func (bus *eventBus) AwaitPublish(e Event) bool {
+	return bus.publish(e, true)
+}
+
+func (bus *eventBus) publish(e Event, await bool) bool {
+	if await {
+		return bus.publishLogic(e)
+	} else {
+		go bus.publishLogic(e)
+	}
+
+	return true
+}
+
+func (bus *eventBus) publishLogic(e Event) bool {
+	bus.mu.Lock()
+	defer bus.mu.Unlock()
+
+	if current, ok := bus.data[e.Type]; !ok {
+		// no previous bind/publish
+		cbSig := getFSignature(e.Data)
+		bus.data[e.Type] = eventBusData{nil, &e, cbSig}
+	} else {
+		current.recent = &e
+		bus.data[e.Type] = current
+	}
+
+	// execute all callbacks for this event
+	prevFile, line := getPreviousCallerFile()
+	log.Println("Publish event ", e, " from ", prevFile, ":", line)
+	if current := bus.data[e.Type]; current.callbacks != nil {
+		for _, cb := range current.callbacks {
+			// check if event data matches expected callback arg type
+			argType := getFSignature(e.Data)
+			if argType != current.cbType {
+				log.Println("Error : Event data type does not match callback arg type.")
+				return false
 			}
-		}
-		bus.Mu.Unlock()
 
-		// continue with the next publish
-		bus.WaitListMu.Lock()
-		next := id + 1
-		nextch, ok := bus.WaitList[next]
-		if !ok {
-			nextch = make(chan bool, 10)
-			bus.WaitList[next] = nextch
+			cbv := reflect.ValueOf(cb)
+			in := []reflect.Value{}
+			if e.Data != nil {
+				arg := reflect.ValueOf(e.Data)
+				in = append(in, arg)
+			}
+			cbv.Call(in)
 		}
-		nextch <- true
-		bus.WaitListMu.Unlock()
-	}()
+	}
+
+	return true
+}
+
+func getFSignature(arg any) reflect.Type {
+	cbArgType := reflect.TypeOf(arg)
+	in := []reflect.Type{}
+	if cbArgType != nil {
+		in = append(in, cbArgType)
+	}
+	out := []reflect.Type{}
+	fSig := reflect.FuncOf(in, out, false)
+	return fSig
 }
 
 func getPreviousCallerFile() (string, int) {
