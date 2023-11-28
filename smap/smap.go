987ba7613173d1263716adc6c625e5787a08665a
yesoer <luisenglaender@web.de>
2023-11-28 16:40:06 +0100

    feat(utils): smap add update functionality
    using this pattern concurrently leads to the lost update problem :
    
    val := Load(key)
    // ... modify val
    Store(key, val)
    
    this function is a simple fix to that issue

--- smap/smap.go
+++ smap/smap.go
@@ -11,6 +11,8 @@ import (
 type SMap[K comparable, V any] interface {
 	Load(key K) (V, bool)
 	Store(key K, value V)
+	Update(key K, modifier func(value V) V) bool
+	Delete(key K)
 }
 
 type smap[K comparable, V any] struct {
@@ -30,6 +32,9 @@ func (s *smap[K, V]) Load(key K) (V, bool) {
 	return v, ok
 }
 
+// This function overwrites whatever is stored under "key", be careful with
+// overwriting because it could lead to data loss. You probably only want to
+// use this for the first write. Else consider Update()
 func (s *smap[K, V]) Store(key K, value V) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -40,4 +45,18 @@ func (s *smap[K, V]) Delete(key K) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	delete(s.m, key)
+}
+
+// Define a modifier function to update the value under K
+func (s *smap[K, V]) Update(key K, modifier func(value V) V) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if v, ok := s.m[key]; ok {
+		newV := modifier(v)
+		s.m[key] = newV
+		return true
+	}
+
+	return false
 }
