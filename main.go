acde52c192e4de3443c34b396f6ce3d891839540
yesoer <71628012+yesoer@users.noreply.github.com>
2024-05-30 18:43:47 +0200

    refactor(main): init temp projects for examples and clean them up

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"distributed-sys-emulator/bus"
 	"distributed-sys-emulator/core"
+	"distributed-sys-emulator/embed"
 	fynegui "distributed-sys-emulator/fynegui"
 	"distributed-sys-emulator/log"
 	"flag"
+	"os"
 )
 
 func main() {
 	flag.Parse()
 
+	log.Info("Setup")
+	tmpExamplesDir := embed.InitEmbeddedExamples()
 	eb := bus.NewEventbus()
 
 	log.Info("Init Core")
@@ -19,4 +23,10 @@ func main() {
 
 	log.Info("Run GUI")
 	fynegui.RunGUI(eb)
+
+	log.Info("Cleanup")
+	err := os.RemoveAll(tmpExamplesDir)
+	if err != nil {
+		log.Error(err)
+	}
 }
