fce57dd32d824f1cfdd3ec20ad8cc8b05405d508
yesoer <71628012+yesoer@users.noreply.github.com>
2024-05-30 18:48:16 +0200

    feat(nvim): add a simple nvim widget wrapper

---
+++ fynegui/nvim.go
@@ -0,0 +1,36 @@
+package fynegui
+
+import (
+	"distributed-sys-emulator/log"
+	"os"
+
+	"fyne.io/fyne/v2"
+	nvim "github.com/yesoer/fyne-nvim"
+)
+
+// Declare conformance with the EditorType interface
+var _ EditorType = (*Nvim)(nil)
+
+// Extension of the NeoVim widget
+type Nvim struct {
+	*nvim.NeoVim
+}
+
+// Create a new instance of the Nvim wrapper
+func NewNvim(dirPth string) EditorType {
+	log.Debug("Start Nvim at path: ", dirPth)
+
+	if _, err := os.ReadDir(dirPth); err != nil {
+		dirPth = "./"
+	}
+
+	neovim := nvim.New(dirPth)
+	neovimWrap := &Nvim{neovim}
+	neovimWrap.ExtendBaseWidget(neovimWrap)
+	return neovimWrap
+}
+
+// TypedShortcut implements the Shortcutable interface
+func (e *Nvim) TypedShortcut(sc fyne.Shortcut) {
+	log.Debug("Typed Shortcut Nvim")
+}
