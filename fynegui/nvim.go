f03fa92e8520c43741362ab0f812c0f62cc75d4d
yesoer <71628012+yesoer@users.noreply.github.com>
2024-07-06 21:19:04 +0200

    fix(nvim): don't quit last window
    Before using e.g. :q when only one window was left would crash the widget. Now q is substituted by Ex. This may not be the final fix.

--- fynegui/nvim.go
+++ fynegui/nvim.go
@@ -1,11 +1,11 @@
 package fynegui
 
 import (
-	"github.com/yesoer/p2p-sim/log"
 	"os"
 
-	"fyne.io/fyne/v2"
-	nvim "github.com/yesoer/fyne-nvim"
+	"github.com/yesoer/p2p-sim/log"
+
+	fynenvim "github.com/yesoer/fyne-nvim"
 )
 
 // Declare conformance with the EditorType interface
@@ -13,7 +13,7 @@ var _ EditorType = (*Nvim)(nil)
 
 // Extension of the NeoVim widget
 type Nvim struct {
-	*nvim.NeoVim
+	*fynenvim.NeoVim
 }
 
 // Create a new instance of the Nvim wrapper
@@ -24,13 +24,48 @@ func NewNvim(dirPth string) EditorType {
 		dirPth = "./"
 	}
 
-	neovim := nvim.New(dirPth)
+	neovim := fynenvim.New(dirPth)
+
+	// if only one window is open prevent quitting it as it crashes the widget
+	smart_quit_vim := `
+						" quit single window 
+						" -> if only one window : go into explorer
+						cnoreabbrev <expr> q winnr('$') <= 1 ? 'Ex' : 'q'
+						cnoreabbrev <expr> quit winnr('$') <= 1 ? 'Ex' : 'quit'
+
+						" (if changed) save window and quit
+						" -> if only one window : save and go into explorer
+						command! WEx execute 'w | Ex'
+						cnoreabbrev <expr> wq winnr('$') <= 1 ? 'WEx' : 'wq'
+						cnoreabbrev <expr> x winnr('$') <= 1 ? 'WEx' : 'x'
+						cnoreabbrev <expr> exit winnr('$') <= 1 ? 'WEx' : 'exit'
+						
+						" quit all windows 
+						" -> close all but one and go into explorer
+						command! OnlyEx execute 'only | Ex'
+						cnoreabbrev quitall OnlyEx
+						cnoreabbrev qall OnlyEx
+						cnoreabbrev qa OnlyEx
+						
+						" (if changed) save all and quit all windows
+						" -> save all and close all but one and go into explorer
+						command! WAllOnlyEx execute 'wa | OnlyEx'
+						cnoreabbrev xall WAllOnlyEx
+						cnoreabbrev xa WAllOnlyEx
+						cnoreabbrev wqa WAllOnlyEx
+						cnoreabbrev wqall WAllOnlyEx
+
+						" normal mode
+						nnoremap ZZ :w<CR>:Ex<CR>
+						nnoremap ZQ :Ex<CR>
+					  `
+
+	_, err := neovim.Engine.Exec(smart_quit_vim, make(map[string]interface{}))
+	if err != nil {
+		log.Error(err)
+	}
+
 	neovimWrap := &Nvim{neovim}
 	neovimWrap.ExtendBaseWidget(neovimWrap)
 	return neovimWrap
-}
-
-// TypedShortcut implements the Shortcutable interface
-func (e *Nvim) TypedShortcut(sc fyne.Shortcut) {
-	log.Debug("Typed Shortcut Nvim")
 }
